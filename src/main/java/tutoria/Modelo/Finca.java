c7f698d9efd85ab556e039e7c5841ff5a6788a59
Luis Eduardo Niño Perez <luisenino@hotmail.com>
2021-10-31 21:12:33 -0500

    First commit

--- src/main/java/tutoria/Modelo/Finca.java
+++ src/main/java/tutoria/Modelo/Finca.java
@@ -18,93 +18,165 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
- *
- * @author user
+ * Control de Fincas
+ * @author Luis Eduardo Niño Pérez
  */
 @Entity
 @Table(name = "farm")
 public class Finca implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+    * Identificador de la Finca
+     */
     private Integer id;
+    /**
+    * Nombre de la Finca
+     */
     private String name; 
+    /**
+    * Dirección de la Finca
+     */
     private String address;
-    private Integer  extension; 
+    /**
+    * Extensión de la Finca
+     */
+    private Integer  extension;
+    /**
+    * Descripción de la Finca
+     */
     private String description;
     
     @ManyToOne
     @JoinColumn(name="categoryid")
     @JsonIgnoreProperties("farms")
+    /**
+    * Categoría de la Finca
+     */
     private Categoria category;
     
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
     @JsonIgnoreProperties({"farm","client"})
+    /**
+    * Mensaje de la Finca
+     */
     private List<Mensaje> messages;
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "farm")
     @JsonIgnoreProperties({"farm","message"})
+    /**
+    * Reservación de la Finca
+     */
     public List<Reservacion> reservations;
 
+    /**
+    * Get del Id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+    * Set del Id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+    * Get del Name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+    * Set del Name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+    * Get del Adress
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+    * Set del Adress
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+    * Get del Extension
+     */
     public Integer getExtension() {
         return extension;
     }
 
+    /**
+    * Set del Extension
+     */
     public void setExtension(Integer extension) {
         this.extension = extension;
     }
 
+    /**
+    * Get del Description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+    * Set del Description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+    * Get del Category
+     */
     public Categoria getCategory() {
         return category;
     }
 
+    /**
+    * Set del Category
+     */
     public void setCategory(Categoria category) {
         this.category = category;
     }
 
+    /**
+    * Get del Messages
+     */
     public List<Mensaje> getMessages() {
         return messages;
     }
 
+    /**
+    * Set del Messages
+     */
     public void setMessages(List<Mensaje> messages) {
         this.messages = messages;
     }
 
+    /**
+    * Get del Reservations
+     */
     public List<Reservacion> getReservations() {
         return reservations;
     }
 
+    /**
+    * Set del reservations
+     */
     public void setReservations(List<Reservacion> reservations) {
         this.reservations = reservations;
     }
--- src/main/java/tutoria/Web/ReservacionWeb.java
+++ src/main/java/tutoria/Web/ReservacionWeb.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ReservacionWeb {
     @Autowired
     private ServiciosReservacion servicio;
